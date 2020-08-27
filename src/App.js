c709adb9aae16ad22bf28f0c89a4403aed683490
alvinkim <alvinkim01@naver.com>
2020-08-27 16:55:14 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import Header from './Layout/Header';
+import Navigation from './Layout/Navigation';
+import Router from './Routes/Router';
+import Store from './Store/store';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+        logged: false,
+        onLogin: this.onLogin,
+        onLogout: this.onLogout
+    }
 }
+
+// Login Func
+onLogin = () => {
+    this.setState({
+        logged: true
+    });
+}
+
+// Logout Func
+onLogout = () => {
+    this.setState({
+        logged: false
+    });
+}
+  render() {
+    const { logged, onLogout } = this.state;
+
+    return (
+      <Store.Provider value={this.state}>
+        <Layout>
+          <Header logged={logged} onLogout={onLogout}/>
+          <Navigation />
+          <Content>
+            <Router />
+          </Content>
+        </Layout>
+      </Store.Provider>
+    );
+  }
+}
+
+const Layout = styled.div`
+  margin: 0 auto;
+  display: flex;
+  width: 100%;
+  flex-flow: row wrap;
+`
+const Content = styled.div`
+  margin: 0 auto;
+`
 
 export default App;
